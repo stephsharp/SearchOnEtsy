2edd6d706b62af1c0826a13c8a4f739bcff26f2e
Stephanie Sharp <ssharp@odecee.com.au>
2016-02-10 12:32:17 +1100

    Implement simple currency formatting for listing price

--- EtsySearch/Listings/Models/ETListingCard.m
+++ EtsySearch/Listings/Models/ETListingCard.m
@@ -26,11 +26,39 @@
     _title = _listing.title;
     _shopName = _listing.shopName;
     _mainImageURL = _listing.mainImageURL;
-    _formattedPrice = [NSString stringWithFormat:@"%@ %@", _listing.currencyCode, _listing.price];
+    _formattedPrice = [self formatPrice:_listing.price withCurrencyCode:_listing.currencyCode];
 
     if (_listing.mainImageHexCode) {
         _mainImageColor = [UIColor et_colorFromHexCode:_listing.mainImageHexCode];
     }
+}
+
+- (NSString *)formatPrice:(NSString *)price withCurrencyCode:(NSString *)currencyCode
+{
+    NSDecimalNumber *amount = [NSDecimalNumber decimalNumberWithString:price];
+    NSNumberFormatter *currencyFormatter = [self currencyFormatterForCurrencyCode:currencyCode];
+
+    return [currencyFormatter stringFromNumber:amount];
+}
+
+- (NSNumberFormatter *)currencyFormatterForCurrencyCode:(NSString *)currencyCode
+{
+    NSNumberFormatter *currencyFormatter = [[NSNumberFormatter alloc] init];
+    [currencyFormatter setNumberStyle:NSNumberFormatterCurrencyStyle];
+
+    NSLocale *locale = [self localeForCurrencyCode:currencyCode];
+    [currencyFormatter setLocale:locale];
+
+    return currencyFormatter;
+}
+
+- (NSLocale *)localeForCurrencyCode:(NSString *)currencyCode
+{
+    NSDictionary *components = @{ NSLocaleCurrencyCode: currencyCode,
+                                  NSLocaleLanguageCode: [NSLocale preferredLanguages][0] };
+    NSString *localeIdentifier = [NSLocale localeIdentifierFromComponents:components];
+
+    return [[NSLocale alloc] initWithLocaleIdentifier:localeIdentifier];
 }
 
 @end
